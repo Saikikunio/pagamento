b012be660a7c71f68931f1853286b03045bf97ef
Saikikunio <samuelpinheironogueira1pessoal@gmail.com>
2025-09-01 15:07:53 -0300

    commit final do projeto da aula

--- app.py
+++ app.py
@@ -62,7 +62,14 @@ def pix_confirmation():
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
+    
+    if not payment:
+        return render_template('404.html'), 404
 
+    if payment.paid:
+        return render_template('confirmed_payment.html',
+                               payment_id=payment_id,
+                               value=payment.value)
     return render_template('payment.html', payment_id=payment_id,
                                            value=payment.value,
                                            host="http://localhost:5000",
@@ -70,7 +77,12 @@ def payment_pix_page(payment_id):
 
 @socketio.on('connect')
 def handle_connect():
-    print("Client connected to the server") 
+    print("Client connected to the server")
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print("Client disconnected from the server")
+    
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
